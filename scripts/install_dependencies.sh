672fdd1c42d86da2e12f087ace2c55bbf0ad4537
crud0626 <sisofiy6048@gmail.com>
2025-08-12 22:42:33 +0900

    fix : shell script 수정
    - 불필요한 script 구문 삭제
    
    - .env 복사 누락

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,6 +6,7 @@ sudo chown -R nginx:nginx /var/www/html
 sudo find /var/www/html -type d -exec chmod 755 {} \;
 sudo find /var/www/html -type f -exec chmod 644 {} \;
 
+# SELinux가 활성화된 경우 컨텍스트 설정
 if command -v getenforce &> /dev/null && [[ $(getenforce) != "Disabled" ]]; then
     sudo setsebool -P httpd_can_network_connect 1
     sudo restorecon -R /var/www/html
